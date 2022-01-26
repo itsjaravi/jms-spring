ac8d3e8cb81e3f8821dd6490c319121fea9dc883
Jayaramachandiran Ravi <60870206+itsjaravi@users.noreply.github.com>
2022-01-26 10:49:55 +0530

    Add files via upload

---
+++ messaging-jms/src/main/java/com/jms/jay/messagingjms/service/Receiver.java
@@ -0,0 +1,15 @@
+package com.jms.jay.messagingjms.service;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class Receiver {
+	
+	@JmsListener(destination = "mailbox",containerFactory = "myFactory")
+	public void ReceiveMessage (Email email) {
+		System.out.println("Received <"+email+">");
+	}
+
+}
